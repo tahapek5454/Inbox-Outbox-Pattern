71f8d20483cce8d108c4968646d2a7435823d406
tahapek5454 <tahapek5454@gmail.com>
2024-01-28 12:35:26 +0300

    feat: add Idempotent desing

--- StokService/Consumers/OrderCreatedEventConsumer.cs
+++ StokService/Consumers/OrderCreatedEventConsumer.cs
@@ -15,16 +15,20 @@ namespace StokService.Consumers
     {
         public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
         {
-
-            await _dbContext.OrderInboxes.AddAsync(new()
+            // Idempotent tasarımını gerceklestirdik, bir terslik olur da tekrar eden event gelirse Inbox Table a eklenemeyecek
+            if (!_dbContext.OrderInboxes.Any(x => x.IdempotentToken.Equals(context.Message.IdempotentToken)))
             {
-                Payload = JsonSerializer.Serialize(context.Message),
-                Processed = false
-            });
+                await _dbContext.OrderInboxes.AddAsync(new()
+                {
+                    IdempotentToken = context.Message.IdempotentToken,
+                    Payload = JsonSerializer.Serialize(context.Message),
+                    Processed = false
+                });
 
-            await _dbContext.SaveChangesAsync();
+                await _dbContext.SaveChangesAsync();
 
-
+            }
+                
             // Yine bu asamayi da OrderOutbox Table'daki gibi belirli periyotlarla
             // Inbox Table'i kontrol edecek sekilde tasarlanmalidir. Simdilik boyle kalsin :)
 
